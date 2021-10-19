7577ca6d8fad302b27e829560c7c19acbc91bbd7
ryokosuge <ryo.kosuge@gmail.com>
2021-10-19 11:56:14 +0900

    chore:	Writing constructs - Define the HitCounter API

---
+++ lib/hitcounter.ts
@@ -0,0 +1,17 @@
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+
+export interface HitCounterProps {
+	// the function for which we want to count url his
+	downstream: lambda.IFunction;
+}
+
+export class HitCounter extends cdk.Construct {
+
+	constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
+		super(scope, id);
+
+		// TODO
+	}
+
+}
