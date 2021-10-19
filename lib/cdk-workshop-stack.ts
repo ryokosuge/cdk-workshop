c69c1889605c62a1c273d60098178aba18382bfc
ryokosuge <ryo.kosuge@gmail.com>
2021-10-19 13:50:56 +0900

    chore:	Writing constructs - Use the hit counter

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as apiGateway from '@aws-cdk/aws-apigateway';
+import { HitCounter } from './hitcounter';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -13,9 +14,13 @@ export class CdkWorkshopStack extends cdk.Stack {
       handler: 'hello.handler',               // file is "hello", fuction is "handler"
     });
 
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello,
+    });
+
     // defines an API Gateway REST API resource backed by our "hello" function.
     new apiGateway.LambdaRestApi(this, 'Endpoint', {
-      handler: hello,
+      handler: helloWithCounter.handler,
     });
   }
 }
